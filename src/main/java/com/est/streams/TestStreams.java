6e9d8bb75a3202aaecfed2c11749823006a5f2cb
Estevam Meneses <Estevam Meneses@localhost>
2021-04-15 11:21:44 -0400

    more updates

--- src/main/java/com/est/streams/TestStreams.java
+++ src/main/java/com/est/streams/TestStreams.java
@@ -4,20 +4,62 @@
  */
 package com.est.streams;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author estevam
  */
 public class TestStreams {
-    
-    
-    
-    public static void main(String ards[]){
 
-    
-    }
-    
-    public void readTextFile(){
-    }
-    
+	private static MyStreams test = new MyStreams();
+
+	public static void main(String ards[]) {
+		test.readTextFile();
+		test.readPerson();
+	}
+
+	static class MyStreams {
+
+		public MyStreams() {
+		}
+
+		public void readTextFile() {
+			List<String> list = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+	 		list.stream().filter(s -> s.startsWith("c")).map(String::toUpperCase).sorted().forEach(System.out::println);
+	
+		}
+
+		public void readPerson() {
+	        /*		
+	        List<Person> persons = new ArrayList<Person>();
+			persons.add(new Person("Peter", 23));
+			persons.add(new Person ("Pamela", 23));
+			persons.add(new Person("David", 12));
+			List<Person> filtered = persons.stream().filter(p -> p.name.startsWith("P")).collect(Collectors.toList());
+
+			System.out.println(filtered);
+			*/
+
+		}
+
+	}
+
+	class Person {
+		String name;
+		int age;
+
+		Person(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
+
 }
---
+++ src/main/java/com/est/threads/future/Main.java
@@ -0,0 +1,11 @@
+package com.est.threads.future;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(
+				("Number of processors: " + Runtime.getRuntime().availableProcessors()));
+	}
+
+}
